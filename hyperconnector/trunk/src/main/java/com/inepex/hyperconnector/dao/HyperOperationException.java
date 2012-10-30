9f8935af6af769ca41e9197ad1d9d11e36013688
sebestyen.csorba@inepex.com <sebestyen.csorba@inepex.com@3b53a2a0-4c8c-e6a7-c89d-e179a8bce7b6>
2012-10-30 14:11:23 +0000

    moving HyperDumper here; adding some tests to hyperconnectorexample;

--- hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/dao/HyperOperationException.java
+++ hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/dao/HyperOperationException.java
@@ -1,9 +1,23 @@
 package com.inepex.hyperconnector.dao;
 
 public class HyperOperationException extends Exception {
+	
 	private static final long serialVersionUID = 4459264579039013328L;
+	
+	public static final String hyperPoolArgsNull = "HyperDaoBase error: hyperPoolArgs null.";
+	public static final String hyperClientPoolNull = "HyperDaoBase error: hyperPoolArgs.getHyperClientPool() returned null.";
+	public static final String hyperHqlServicePoolNull = "HyperDaoBase error: hyperPoolArgs.getHyperHqlServicePool() returned null.";
+	public static final String hyperOperationFailed = "Hypertable operation failed.";
+	public static final String hyperOperationFailed_ResourceCreation = "Hypertable operation failed: resource creation error.";
+	public static final String hyperOperationFailed_ClientException = "Hypertable operation failed: ClientException.";
+	public static final String hyperOperationFailed_ThriftCommError = "Hypertable operation failed: Thrift communication error.";
+	public static final String hyperOperationFailed_Interrupted = "Hypertable operation failed: interrupted.";
 
 	public HyperOperationException(String message) {
 		super(message);
+	}
+
+	public HyperOperationException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
