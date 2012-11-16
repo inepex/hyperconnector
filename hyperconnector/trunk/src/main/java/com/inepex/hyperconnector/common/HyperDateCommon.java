60979ece6148e121a8d5a89f8f60eb0fc7ceae88
tibor.somodi <tibor.somodi@inepex.com>
2012-11-16 14:11:57 +0000

    revert HyperDateCommon

--- hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/common/HyperDateCommon.java
+++ hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/common/HyperDateCommon.java
@@ -1,5 +1,7 @@
 package com.inepex.hyperconnector.common;
 
+import java.util.Date;
+
 
 /**
  * Class for commonly used functions.
@@ -13,7 +15,27 @@ public class HyperDateCommon {
 	 * time of the cell in nanoseconds since the epoch".
 	 */
 
-	public static long utcNowInNanoSecs() {
-		return System.nanoTime();
-	}
+	  private static final long MS_TO_NS_MULTIPLIER = 1000000l;
+      public static long nanoSecsToMilliSecs(long nanoSecs) {
+              return nanoSecs / MS_TO_NS_MULTIPLIER;
+      }
+
+      public static long milliSecsToNanoSecs(long milliSecs) {
+              return milliSecs * MS_TO_NS_MULTIPLIER;
+      }
+
+      public static Date nanoSecsTimestampToDate(long timestamp) {
+              return new Date(nanoSecsToMilliSecs(timestamp));
+      }
+     
+      public static long dateToNanoSecsTimestamp(Date date) {
+              if (date == null)
+                      return 0l;
+              return milliSecsToNanoSecs(date.getTime());
+      }
+
+      public static long utcNowInNanoSecs() {
+              return milliSecsToNanoSecs(System.currentTimeMillis());
+      }
+
 }
