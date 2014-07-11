0996ed41eaa8bf35f4d512d4b09aa1d79fe759cb
sebestyen.csorba@inepex.com <sebestyen.csorba@inepex.com@3b53a2a0-4c8c-e6a7-c89d-e179a8bce7b6>
2014-07-11 13:16:38 +0000

    fix in help text, host param handling

--- hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/dumpreader/HyperDumpRestoreApp.java
+++ hyperconnector/trunk/src/main/java/com/inepex/hyperconnector/dumpreader/HyperDumpRestoreApp.java
@@ -76,8 +76,8 @@ public class HyperDumpRestoreApp {
 	}
 
 	private static void createHyperResources() {
-		HyperClientPool hyperClientPool = new HyperClientPool("localhost", 38080);
-		HyperHqlServicePool hyperHqlServicePool = new HyperHqlServicePool("localhost", 38080);
+		HyperClientPool hyperClientPool = new HyperClientPool(hyperAddress, hyperPort);
+		HyperHqlServicePool hyperHqlServicePool = new HyperHqlServicePool(hyperAddress, hyperPort);
 		
 		hyperPoolArgs = new HyperPoolArgs(hyperClientPool, 
 				hyperHqlServicePool, 
@@ -113,9 +113,9 @@ public class HyperDumpRestoreApp {
 				"\n" +
 				
 				"minimal params to invoke :\n" +
-				"\tHyperDumpRestoreApp hyperAddress=beta-app.inetrack.com hyperAddress=localhost hyperPort=38080 baseFolder=/backup/hyperDump/ nameSpace=InepexLbs table=Report\n\n"+
+				"\tHyperDumpRestoreApp hyperAddress=localhost hyperPort=38080 baseFolder=/backup/hyperDump/ nameSpace=InepexLbs table=Report\n\n"+
 				"sample invoke :\n" +
-				"\tHyperDumpRestoreApp hyperAddress=beta-app.inetrack.com hyperAddress=localhost hyperPort=38080 baseFolder=/backup/hyperDump/ nameSpace=InepexLbs table=Report year=2012 month=12 day=12-25 content=BOTH\n"
+				"\tHyperDumpRestoreApp hyperAddress=localhost hyperPort=38080 baseFolder=/backup/hyperDump/ nameSpace=InepexLbs table=Report year=2012 month=12 day=12-25 content=BOTH\n"
 		);
 	}
 	
